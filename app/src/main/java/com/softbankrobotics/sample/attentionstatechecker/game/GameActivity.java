61289f9d0f7f86f19eea9d6caeb7a1b41fcadc6e
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2018-05-30 17:04:51 +0200

    GameActivity - Implement robot lifecycle

--- app/src/main/java/com/softbankrobotics/sample/attentionstatechecker/game/GameActivity.java
+++ app/src/main/java/com/softbankrobotics/sample/attentionstatechecker/game/GameActivity.java
@@ -8,13 +8,39 @@ package com.softbankrobotics.sample.attentionstatechecker.game;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
 import com.softbankrobotics.sample.attentionstatechecker.R;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements RobotLifecycleCallbacks {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        QiSDK.register(this, this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        QiSDK.unregister(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+        // TODO: impl
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+        // TODO: impl
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+        // Not used.
     }
 }
