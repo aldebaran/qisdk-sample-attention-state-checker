5742c95c0a1c75b5013d1e06e66a6e03f5502846
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2018-05-30 17:01:16 +0200

    Create GameActivity

---
+++ app/src/main/java/com/softbankrobotics/sample/attentionstatechecker/game/GameActivity.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2018 Softbank Robotics Europe
+ * See COPYING for the license
+ */
+
+package com.softbankrobotics.sample.attentionstatechecker.game;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.softbankrobotics.sample.attentionstatechecker.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
