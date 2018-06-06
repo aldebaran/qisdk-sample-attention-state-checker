4abe464488ec693602dac8f7da6de0c755dc4811
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2018-06-06 09:05:46 +0200

    Convert GameEvent to Kotlin

---
+++ app/src/main/java/com/softbankrobotics/sample/attentionstatechecker/game/GameEvent.kt
@@ -0,0 +1,14 @@
+package com.softbankrobotics.sample.attentionstatechecker.game
+
+/**
+ * A game event.
+ */
+internal enum class GameEvent {
+    FOCUS_GAINED,
+    FOCUS_LOST,
+    INTRO_FINISHED,
+    INSTRUCTIONS_FINISHED,
+    MATCH,
+    MATCHING_FINISHED,
+    NOT_MATCHING_FINISHED
+}
