55ec59c02234628425b1b082fe1357d44e96bac3
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2018-06-06 14:27:15 +0200

    Convert HumanData and MyOperators to Kotlin

---
+++ app/src/main/java/com/softbankrobotics/sample/attentionstatechecker/model/data/HumanData.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2018 Softbank Robotics Europe
+ * See COPYING for the license
+ */
+
+package com.softbankrobotics.sample.attentionstatechecker.model.data
+
+import com.aldebaran.qi.sdk.`object`.human.AttentionState
+import com.aldebaran.qi.sdk.`object`.human.Human
+
+/**
+ * Hold a [Human] data.
+ */
+data class HumanData(val human: Human, val attentionState: AttentionState, val distance: Double)
---
+++ app/src/main/java/com/softbankrobotics/sample/attentionstatechecker/model/rx/operator/MyOperators.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2018 Softbank Robotics Europe
+ * See COPYING for the license
+ */
+
+@file:JvmName("MyOperators")
+
+package com.softbankrobotics.sample.attentionstatechecker.model.rx.operator
+
+import com.aldebaran.qi.sdk.`object`.human.AttentionState
+import com.aldebaran.qi.sdk.`object`.human.AttentionState.*
+import com.softbankrobotics.sample.attentionstatechecker.model.data.Direction
+import com.softbankrobotics.sample.attentionstatechecker.model.data.Direction.*
+import com.softbankrobotics.sample.attentionstatechecker.model.data.HumanData
+import com.softbankrobotics.sample.attentionstatechecker.model.data.Wrapper
+import io.reactivex.Observable
+
+fun closest(observable: Observable<List<HumanData>>): Observable<Wrapper<HumanData>> {
+    return observable.map { humanDataList ->
+        val min = humanDataList.minBy { it.distance }
+
+        if (min != null) {
+            Wrapper.of(min)
+        } else {
+            Wrapper.empty<HumanData>()
+        }
+    }
+}
+
+fun attentionState(observable: Observable<Wrapper<HumanData>>): Observable<Wrapper<AttentionState>> {
+    return observable.map { wrapper ->
+        if (wrapper.hasContent()) {
+            Wrapper.of(wrapper.content.attentionState)
+        } else {
+            Wrapper.empty<AttentionState>()
+        }
+    }
+}
+
+fun direction(observable: Observable<Wrapper<AttentionState>>): Observable<Direction> {
+    return observable.map { wrapper ->
+        if (!wrapper.hasContent()) {
+            return@map Direction.UNKNOWN
+        }
+
+        val attentionState = wrapper.content
+
+        when (attentionState) {
+            LOOKING_UP -> UP
+            LOOKING_DOWN -> DOWN
+            LOOKING_LEFT -> LEFT
+            LOOKING_RIGHT -> RIGHT
+            LOOKING_UP_LEFT -> UP_LEFT
+            LOOKING_UP_RIGHT -> UP_RIGHT
+            LOOKING_DOWN_LEFT -> DOWN_LEFT
+            LOOKING_DOWN_RIGHT -> DOWN_RIGHT
+            else -> Direction.UNKNOWN
+        }
+    }
+}
