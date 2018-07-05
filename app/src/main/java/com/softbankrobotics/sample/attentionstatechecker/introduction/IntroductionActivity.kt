ea30389c1b846a24d41e2110bcf85b0ee7585ffa
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2018-07-05 15:46:29 +0200

    Add timer for introduction

--- app/src/main/java/com/softbankrobotics/sample/attentionstatechecker/introduction/IntroductionActivity.kt
+++ app/src/main/java/com/softbankrobotics/sample/attentionstatechecker/introduction/IntroductionActivity.kt
@@ -11,13 +11,23 @@ import com.aldebaran.qi.sdk.builder.QiChatbotBuilder
 import com.aldebaran.qi.sdk.builder.TopicBuilder
 import com.aldebaran.qi.sdk.design.activity.RobotActivity
 import com.softbankrobotics.sample.attentionstatechecker.R
+import io.reactivex.Single
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_introduction.*
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicBoolean
 
 class IntroductionActivity : RobotActivity(), RobotLifecycleCallbacks {
 
     private var chat: Chat? = null
     private var qiChatbot: QiChatbot? = null
     private var startBookmark: Bookmark? = null
+    private var secondBookmark: Bookmark? = null
+
+    private var timerDisposable: Disposable? = null
+    private val shouldRepeatWithTimer = AtomicBoolean(true)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,12 +53,15 @@ class IntroductionActivity : RobotActivity(), RobotLifecycleCallbacks {
     }
 
     override fun onRobotFocusGained(qiContext: QiContext?) {
+        shouldRepeatWithTimer.set(true)
+
         val topic = TopicBuilder.with(qiContext)
                         .withResource(R.raw.introduction)
                         .build()
 
         val bookmarks = topic.bookmarks
         val firstBookmark = bookmarks[FIRST_BOOKMARK]
+        secondBookmark = bookmarks[SECOND_BOOKMARK]
         startBookmark = bookmarks[START_BOOKMARK]
 
         qiChatbot = QiChatbotBuilder.with(qiContext)
@@ -59,6 +72,8 @@ class IntroductionActivity : RobotActivity(), RobotLifecycleCallbacks {
             when (it.name) {
                 SECOND_BOOKMARK -> displayStartButton()
                 START_BOOKMARK -> checkStartButton()
+                START_TIMER_BOOKMARK -> if (shouldRepeatWithTimer.getAndSet(false)) startTimer()
+                STOP_TIMER_BOOKMARK -> stopTimer()
             }
         }
 
@@ -72,6 +87,8 @@ class IntroductionActivity : RobotActivity(), RobotLifecycleCallbacks {
     }
 
     override fun onRobotFocusLost() {
+        stopTimer()
+
         qiChatbot?.removeAllOnBookmarkReachedListeners()
         chat?.removeAllOnStartedListeners()
     }
@@ -109,9 +126,25 @@ class IntroductionActivity : RobotActivity(), RobotLifecycleCallbacks {
         qiChatbot?.async()?.goToBookmark(bookmark, AutonomousReactionImportance.HIGH, AutonomousReactionValidity.IMMEDIATE)
     }
 
+    private fun startTimer() {
+        timerDisposable = Single.timer(5, TimeUnit.SECONDS)
+                .observeOn(Schedulers.io())
+                .subscribeOn(Schedulers.io())
+                .subscribe { _ ->
+                    stopTimer()
+                    goToBookmark(secondBookmark)
+                }
+    }
+
+    private fun stopTimer() {
+        timerDisposable?.takeUnless { it.isDisposed }?.dispose()
+    }
+
     private companion object {
         const val FIRST_BOOKMARK = "first"
         const val SECOND_BOOKMARK = "second"
         const val START_BOOKMARK = "start"
+        const val START_TIMER_BOOKMARK = "start_timer"
+        const val STOP_TIMER_BOOKMARK = "stop_timer"
     }
 }
