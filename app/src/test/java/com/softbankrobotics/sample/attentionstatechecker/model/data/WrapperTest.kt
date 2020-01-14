11aa6188bf9d945650bd6f37701aff4c68c8ee30
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2020-01-14 18:07:30 +0100

    Add tests for Wrapper

---
+++ app/src/test/java/com/softbankrobotics/sample/attentionstatechecker/model/data/WrapperTest.kt
@@ -0,0 +1,20 @@
+package com.softbankrobotics.sample.attentionstatechecker.model.data
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class WrapperTest {
+
+    @Test
+    fun empty_has_null_content() {
+        val wrapper = Wrapper.empty<Nothing>()
+        assertNull(wrapper.content)
+    }
+
+    @Test
+    fun not_empty_keeps_content() {
+        val content = 42
+        val wrapper = Wrapper.of(content)
+        assertEquals(content, wrapper.content)
+    }
+}
